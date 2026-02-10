6677bbc39ae54e29e2b79a54cf448273a8ff4917
LicenDrinc <mihaoo4556@gmail.com>
2026-02-10 09:45:42 +0500

    2 2

--- 2/AANotes/AANotes/MainWindow.axaml.cs
+++ 2/AANotes/AANotes/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using System.ComponentModel;
+using System.IO;
 
 namespace AANotes
 {
@@ -8,5 +12,45 @@ namespace AANotes
         {
             InitializeComponent();
         }
+
+        /*
+        private async void OpenFile(object? sender, RoutedEventArgs e)
+        {
+            var files = await this.StorageProvider.OpenFilePickerAsync(
+                new FilePickerOpenOptions
+                {
+                    Title = "Открыть файл",
+                    AllowMultiple = false
+                });
+
+            if (files.Count > 0)
+            {
+                await using var stream = await files[0].OpenReadAsync();
+                using var reader = new StreamReader(stream);
+                Editor.Text = await reader.ReadToEndAsync();
+            }
+        }
+
+        private async void SaveFile(object? sender, RoutedEventArgs e)
+        {
+            var file = await this.StorageProvider.SaveFilePickerAsync(
+                new FilePickerSaveOptions
+                {
+                    Title = "Сохранить файл"
+                });
+
+            if (file != null)
+            {
+                await using var stream = await file.OpenWriteAsync();
+                using var writer = new StreamWriter(stream);
+                await writer.WriteAsync(Editor.Text);
+            }
+        }
+
+        public void OnClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            Title = "дададададада";
+        }
+        */
     }
 }
