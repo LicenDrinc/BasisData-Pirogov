c54e3a166ee9ebad415f4487faccd87ba3ccd850
LicenDrinc <mihaoo4556@gmail.com>
2026-04-02 12:44:15 +0500

    2 21

--- 2/AANotes/AANotes/MainWindow.axaml.cs
+++ 2/AANotes/AANotes/MainWindow.axaml.cs
@@ -87,7 +87,8 @@ namespace AANotes
             for (int i = 0; i < notesList.Count; i++) sql1 += notesList[i].Id + "\t" + notesList[i].Title + "\t" + notesList[i].Text + "\t" + notesList[i].TimeEditor + "\n";
             for (int i = 0; i < linksList.Count; i++) sql2 += linksList[i].Id + "\t" + linksList[i].IdNote + "\t" + linksList[i].Link + "\n";
             sql1 += "\\."; sql2 += "\\."; var sql = sql1 + "\n" + sql2;
-            using var conn = new NpgsqlConnection(adminCs1); conn.Open();
+            Console.WriteLine(sql);
+            using var conn = new NpgsqlConnection(adminCs); conn.Open();
             using var cmd = new NpgsqlCommand(sql, conn); cmd.ExecuteNonQuery();
             UpdateNote(); UpdateLinks(); notesJurnal.Clear(); OpenMain();
         }
